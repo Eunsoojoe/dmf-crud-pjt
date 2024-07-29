1aaf693f4a46ecfaa4d6c4aa00763c550c8000ba
Eunsoojoe <joeun2264@gmail.com>
2024-07-29 11:08:48 +0900

    model 정의

---
+++ README.md
@@ -0,0 +1,4 @@
+RDBMS 
+# Model
+
+객체관계매핑 : python과 sql을 연결
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# DB내 컬럼명 저장
+class Post(models.Model):
+    # 장고 라이브러리에 있는 models.Model을 상속받음.
+    title = models.CharField(max_length=100)    # model안에 char 형이 들어갈 수 있는 변수 생성
+    content = models.TextField(0)
